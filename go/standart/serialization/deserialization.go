4616f90f44222a8b074c8f7df4e28fac1fa79d7f
Oleg Semashko <semashko.olezhka@inbox.ru>
2024-11-24 16:43:43 +0300

    init i know project

---
+++ go/standart/serialization/deserialization.go
@@ -0,0 +1,24 @@
+package main
+
+// Десериализация - обратный процесс восстановления объекта из последовательности байтов или текстового представления
+
+//Пример десериализации
+
+//type GoUser struct {
+//	ID    int
+//	Name  string
+//	Email string
+//}
+//
+//func main() {
+//	jsonData := `{"id":1,"name":"Oleg","email":"email@email.ru"}`
+//
+//	var user GoUser
+//	err := json.Unmarshal([]byte(jsonData), &user)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//
+//	fmt.Println("Переведено в структуру: ", user)
+//}
